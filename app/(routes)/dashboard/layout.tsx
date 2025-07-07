d9043c0d1dcd7879a07cc6bee90043d98af5baed
harry <haris.me97@gmail.com>
2025-07-07 07:07:03 +0400

    Doctors list added to dashboard screen

--- app/(routes)/dashboard/layout.tsx
+++ app/(routes)/dashboard/layout.tsx
@@ -9,7 +9,10 @@ function DashboardLayout({
   return (
     <div>
         <AppHeader/>
-        {children}</div>
+        <div className='px-10 md:px-20 lg:px-40 py-10'>
+            {children}  
+        </div>
+    </div>
   )
 }
 
