5c70c9eda876312849bc26879a4cebcbb000c1e3
harry <haris.me97@gmail.com>
2025-07-27 10:09:35 +0400

    History and home navigation added. with new History page

--- app/(routes)/dashboard/_components/AppHeader.tsx
+++ app/(routes)/dashboard/_components/AppHeader.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import Image from 'next/image'
 import { UserButton } from '@clerk/nextjs'
+import Link from 'next/link'
 
 const menuOptions = [
     {
         id: 1,
         name: 'Home',
-        path: '/home'
+        path: '/dashboard'
     },
     {
         id: 2,
         name: 'History',
-        path: '/history'
+        path: '/dashboard/history'
     },
     {
         id: 3,
@@ -30,10 +31,10 @@ function AppHeader() {
     <div className='flex items-center justify-between p-4 shadow px-10 md:px-20 lg:px-40'>
         <Image src={'/logo.svg'} alt='logo' width={160} height={80} />
         <div className='hidden md:flex gap-12 items-center'>
-            {menuOptions.map((option, index) => (
-                <div key={index}>
-                    <h2 className="hover:font-bold cursor-pointer transition-all">{option.name}</h2>
-                </div>
+            {menuOptions.map((option) => (
+            <Link key={option.id} href={option.path}>
+                <h2 className="hover:font-bold cursor-pointer transition-all">{option.name}</h2>
+            </Link>
             ))}
         </div>
         <UserButton/>
---
+++ app/(routes)/dashboard/history/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import HistoryList from '../_components/HistoryList'
+
+function History() {
+  return (
+    <HistoryList />
+  )
+}
+
+export default History
