7fc84699ab076b44938cf6f37347e2556f4a6e85
harry <haris.me97@gmail.com>
2025-07-29 21:37:01 +0400

    homepage logo change and dashboard button cursor added

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,12 @@ import { FeaturesBentoGrid } from "./_components/FeaturesBentoGrid";
 import { UserButton, useUser } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import Image from 'next/image'
+
 
 export default function HeroSectionOne() {
   return (
-    <div className="relative my-10 flex flex-col items-center justify-center">
+    <div className="relative my-10 flex flex-col items-center justify-center ">
       <Navbar />
       <div className="absolute inset-y-0 left-0 h-full w-px bg-neutral-200/80 dark:bg-neutral-800/80">
         <div className="absolute top-0 h-40 w-px bg-gradient-to-b from-transparent via-blue-500 to-transparent" />
@@ -22,7 +24,7 @@ export default function HeroSectionOne() {
       </div>
       <div className="px-4 py-10 md:py-20">
         <h1 className="relative z-10 mx-auto max-w-4xl text-center text-2xl font-bold text-slate-700 md:text-4xl lg:text-7xl dark:text-slate-300">
-          {"Transform Healthcare with AI Medical Voice Agent"
+          {"Your AI Medical Voice Assistant"
             .split(" ")
             .map((word, index) => (
               <motion.span
@@ -53,9 +55,7 @@ export default function HeroSectionOne() {
           }}
           className="relative z-10 mx-auto max-w-xl py-4 text-center text-lg font-normal text-neutral-600 dark:text-neutral-400"
         >
-          With AI, you can launch your website in hours, not days. Try our best
-          in class, state of the art, cutting edge AI tools to get your website
-          up.
+          Save time, improve patient care, and let AI handle your symptoms and diseases.
         </motion.p>
 
         <Link href={'/sign-in'}>
@@ -72,7 +72,7 @@ export default function HeroSectionOne() {
             }}
             className="relative z-10 mt-8 flex flex-wrap items-center justify-center gap-4"
           >
-            <button className="w-60 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200">
+            <button className="w-60 cursor-pointer transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200">
               Explore Now
             </button>
             
@@ -102,6 +102,7 @@ export default function HeroSectionOne() {
               height={1000}
               width={1000}
             />
+            
           </div>
         </motion.div>
       </div>
@@ -115,8 +116,7 @@ const Navbar = () => {
   return (
     <nav className="flex w-full items-center justify-between border-t border-b border-neutral-200 px-4 py-4 dark:border-neutral-800">
       <div className="flex items-center gap-2">
-        <div className="size-7 rounded-full bg-gradient-to-br from-violet-500 to-pink-500" />
-        <h1 className="text-base font-bold md:text-2xl">Aceternity UI</h1>
+        <Image src={'/logo.svg'} alt='logo' width={160} height={80} />
       </div>
       {!user ?
         <Link href={'/sign-in'}>
@@ -129,7 +129,7 @@ const Navbar = () => {
       <UserButton/>
       
         <Link href={'/dashboard'}>
-          <Button>Dashboards</Button>
+          <Button className="cursor-pointer">Dashboards</Button>
         </Link>
       </div>
       }
